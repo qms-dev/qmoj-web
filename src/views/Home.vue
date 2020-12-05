506f432aa8a355f9dbe864e3db287b8ce5667cdc
waynewang98 <1070546617@qq.com>
2020-12-05 21:39:34 +0800

    1.删除不必要的示例文件 2.新增vue.config.js文件 3.实现antd-vue的按需加载 4.更换stylus为less，以兼容antd按需加载

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    
   </div>
+  <a-button>2131213</a-button>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    
   },
 })
 export default class Home extends Vue {}
